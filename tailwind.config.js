9dbe7a842c350225db982d5b33f58bd36b543dda
Garrett Uffelman <garrett@garrettkeith.com>
2026-02-07 21:56:50 -0500

    Switch from Tailwind CDN to compiled CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
